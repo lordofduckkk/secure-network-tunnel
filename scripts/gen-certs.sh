5472711d70ae19295d6f46c07b0fc64e0a1c2dbf
lordofduckkk <egorwinner2007@yandex.ru>
2026-03-13 01:22:40 +0300

    Update gen-certs.sh

--- scripts/gen-certs.sh
+++ scripts/gen-certs.sh
@@ -13,6 +13,6 @@ openssl genpkey -algorithm RSA -out client.key
 openssl req -new -key client.key -out client.csr -subj "/CN=client"
 openssl x509 -req -in client.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out client.pem -days 365
 
-echo "✅ Сертификаты созданы:"
+echo "Сертификаты созданы:"
 ls -l *.pem *.key
 
